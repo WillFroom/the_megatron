8cddb49820103e4af009aee460a0afccec77100b
Will Froom <will.froom@gmail.com>
2020-05-21 22:01:50 +0100

    Added new classes to split up episode, scene, dialogue parsing etc

--- src/the_megatron/parser/dialogue.py
+++ src/the_megatron/parser/dialogue.py
@@ -1,6 +1,29 @@
+from typing import List, Union
+
 from dataclasses import dataclass
 
 @dataclass
 class Dialogue:
     name: str
     speech: str
+
+@dataclass
+class StageDescription:
+    description: str
+
+@dataclass
+class Scene:
+    directions : List[Union[Dialogue, StageDescription]]
+
+    def add_dialogue(self, name, speech):
+        self.directions.append(Dialogue(name, speech))
+
+    def add_stage_description(self, description):
+        self.directions.append(StageDescription(description))
+
+@dataclass
+class Episode():
+    scenes: List[Scene]
+
+    def add_scene(self):
+        self.scenes.append(Scene([]))
--- src/the_megatron/parser/parser.py
+++ src/the_megatron/parser/parser.py
@@ -3,7 +3,7 @@ from typing import List
 from html.parser import HTMLParser
 from urllib import request
 
-from .dialogue import Dialogue
+from .dialogue import Episode, Scene, Dialogue, StageDescription
 
 class EpisodeParser(HTMLParser):
     TRANSCRIPT_ID = 'Transcript'
@@ -11,7 +11,7 @@ class EpisodeParser(HTMLParser):
         super().__init__()
         self.in_transcript = False
         self.tags: List[str] = []
-        self.dialogue: List[Dialogue] = []
+        self.episode = Episode([])
 
     def handle_starttag(self, tag, attrs):
         if ('id', self.TRANSCRIPT_ID) in attrs:
@@ -26,12 +26,32 @@ class EpisodeParser(HTMLParser):
     def handle_data(self, data):
         if self.in_transcript:
             if self.tags[-1] == 'b':
-                self.dialogue.append(Dialogue(
+                self.add_dialogue(
                     data.split(':')[0],
-                    None))
+                    None
+                )
             elif self.tags[-1] == 'p':
-                if self.dialogue and self.dialogue[-1].speech is None:
-                    self.dialogue[-1].speech = data
+                last_direction = self.current_scene.directions[-1]
+                if isinstance(last_direction, Dialogue):
+                    assert last_direction.speech is None, "ruh-ro!"
+                    last_direction.speech = data.rstrip('\\n').replace('\\', '')
+            elif self.tags[-1] == 'i':
+                self.new_scene(data.strip('[]'))
+
+    @property
+    def current_scene(self) -> Scene:
+        return self.episode.scenes[-1]
+
+    def new_scene(self, stage_description):
+        self.episode.add_scene()
+        self.add_stage_description(stage_description)
+
+    def add_dialogue(self, name, speech):
+        self.current_scene.add_dialogue(name, speech)
+
+    def add_stage_description(self, description):
+        self.current_scene.add_stage_description(description)
+
 
 
 def parse_page(url: str):
@@ -41,4 +61,4 @@ def parse_page(url: str):
     parser = EpisodeParser()
     parser.feed(html_response)
 
-    print(parser.dialogue)
+    print(parser.episode)
